843a631c32e8e70a20dbe59070c9da166daabc2c
RodriguezJoseMaria <143606586+RodriguezJoseMaria@users.noreply.github.com>
2023-10-22 18:53:10 +0200

    updated

--- src/movies.js
+++ src/movies.js
@@ -12,43 +12,53 @@ function getAllDirectors(moviesArray) {
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
     
-    if (moviesArray.length = []) {
+    if (moviesArray === 0 || !moviesArray.director === 'Steven Spielberg') {
     return 0;
-}
+} else {
     const moviesSpielberg = moviesArray.filter (movies => {
         return movies.director  === 'Steven Spielberg' && movies.genre.includes('Drama');
-    });
-    return moviesSpielberg;
+    }).map((item) => item.director);
+    return moviesSpielberg.length;
+}
 }
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
-    
-    const sumScore = moviesArray.reduce((acc, currentValue) => 
-    acc + currentValue.score, 0);
-    const aveScore = sumScore / moviesArray.length;
-    return parseFloat(aveScore.toFixed(2));
 
-    if (moviesArray.score.length === 0) {
-        return 0;
+    if (moviesArray.length === 0) {
+    return 0;
+} else {
+    const sumScore = moviesArray.reduce((acc, currentValue) => {
+    if (typeof currentValue.score === 'number') {
+        return acc + currentValue.score;
+    } else {
+        return acc;
     }
-    const emptyScore = moviesArray.reduce((ac, currentValue) => {
-    return acc + currentValue});
-    return emptyScore / moviesArray.length;
+}, 0);
+    const average = sumScore / moviesArray.length;
+    const avgMathTwoDec = Math.round(average * 100) / 100;
+    return avgMathTwoDec;
+    }
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {
-    const dramaScore = moviesArray.reduce( (acc, currentValue) => {
-    return acc + currentValue.score;
-    }, 0) / moviesArray.length;
-    return parseFloat(dramaScore.toFixed(2));
-
-    const genreDrama = moviesArray.reduce( (acc, currentValue) => {
-    return acc + currentValue.genre.includes('Drama')
-    }, 0)
-    const aveDrama = genreDrama / moviesArray.length;
-    return aveDrama;
+    const dramaScore = moviesArray.filter((item) => {
+        return item.genre.includes('Drama');
+    });
+    if (dramaScore.length === 0) {
+        return 0;
+    }
+    const sumAllScore = dramaScore.reduce((acc, currentValue) => {
+        if (typeof currentValue.score === 'number') {
+            return acc + currentValue.score;
+        } else {
+            return acc;
+        }
+    }, 0);
+    const average = sumAllScore / dramaScore.length;
+    const avgMathTwoDec = Math.round(average * 100) / 100;
+    return avgMathTwoDec;
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
@@ -68,16 +78,12 @@ function orderByYear(moviesArray) {
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {
     // const newAlpha = moviesArray.slice();
-    const newAlpha = [...moviesArray];
-    const alphaStr = newAlpha.map(item => JSON.stringify(item));
-    alphaStr.sort( (a, b) => {
-        if (a.title < b.title) return -1;
-        if (a.title > b.title) return 1;
-    });
-    if (alphaStr.length > 20) {
-        alphaStr.length = 20;
-    };
-    return alphaStr;
+    
+    let newAlpha = moviesArray.map((item) => {
+        return item.title;
+    }).sort().slice(0, 20);
+
+    return newAlpha;
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
